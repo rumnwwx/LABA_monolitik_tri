4adb7b22e3c6c09af610ae245f235e65b2b882ff
рума <elchaninapolina@gmail.com>
2024-12-22 22:50:18 +0700

    регистрация

---
+++ blog/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import User
+
+
+class UserCreatingForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите имя пользователя'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите фамилию'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Введите ваш email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Введите пароль'}))
+    avatar = forms.ImageField(widget=forms.FileInput())
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Такое имя пользователя занято.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Такой адрес электронной почты занят.")
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get("password"))
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", 'email', 'avatar')
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,13 @@ from msilib.schema import ListView
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from .forms import UserCreatingForm
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class Register(generic.CreateView):
+    template_name = 'blog/register.html'
+    form_class = UserCreatingForm
